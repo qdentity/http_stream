d29e6eddf1c4233ff9776e420b4c08c086ebb64f
dvic <info@dvic.io>
2023-07-28 15:07:47 +0200

    Bump deps

--- mix.exs
+++ mix.exs
@@ -35,9 +35,9 @@ defmodule HTTPStream.MixProject do
 
   defp deps do
     [
-      {:castore, "~> 0.1.7", optional: true},
-      {:mint, "~> 1.1.0", optional: true},
-      {:httpoison, "~> 1.7.0", optional: true},
+      {:castore, "~> 0.1 or ~> 1.0", optional: true},
+      {:mint, "~> 1.0", optional: true},
+      {:httpoison, "~> 1.0 or ~> 2.0", optional: true},
       {:credo, "~> 1.5.0-rc.2", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
